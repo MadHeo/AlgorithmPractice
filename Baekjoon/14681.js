e630b004829e52fcc2cc37a50704528df306565d
kwangkki_Heo <tnlfl288@gmail.com>
2022-12-08 00:14:40 +0900

    14681

--- Baekjoon/14681.js
+++ Baekjoon/14681.js
@@ -1,4 +1,11 @@
-const input = require("fs").readFileSync("/dev/stdin").toString();
+const [x ,y] = require("fs").readFileSync(0).toString().trim().split('\n').map(Number);
 
-const x = parseInt(input);
-const y = parseInt(input);
+if (x > 0 && y > 0) {
+    console.log(1);
+    } else if ( x < 0 && y > 0 ) {
+        console.log(2);
+    } else if ( x < 0 && y < 0 ) {
+        console.log(3);
+    } else if ( x > 0 && y < 0 ) {
+        console.log(4);
+    };
